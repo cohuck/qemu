7369f8db01c125e52ae50918c6014d5f832c7d49
Xu Wang <gesaint@linux.vnet.ibm.com>
2015-02-05 18:28:31 +0800

    s390x/virtio-ccw: enable has_dynamic_sysbus
    We have to enable this flag to support dynamically adding devices to the
    sysbus. This change is needed for the the upcoming diag288 watchdog.
    
    Signed-off-by: Xu Wang <gesaint@linux.vnet.ibm.com>
    Acked-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Reviewed-by: David Hildenbrand <dahi@linux.vnet.ibm.com>
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -216,6 +216,7 @@ static void ccw_machine_class_init(ObjectClass *oc, void *data)
     mc->no_sdcard = 1;
     mc->use_sclp = 1;
     mc->max_cpus = 255;
+    mc->has_dynamic_sysbus = true;
     nc->nmi_monitor_handler = s390_nmi;
 }
 
